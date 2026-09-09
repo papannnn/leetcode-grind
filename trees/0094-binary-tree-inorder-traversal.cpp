7b339f3998d74ec82e8791f24daba08fe29b12f0
papannn <taufan.maaruf@gmail.com>
2026-09-09 23:12:05 +0700

    feat: add binary tree inorder traversal

---
+++ trees/0094-binary-tree-inorder-traversal.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root) {
+        if (!root) {
+            return;
+        }
+
+        traverse(root->left);
+        res.push_back(root->val);
+        traverse(root->right);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        traverse(root);
+        return res;
+    }
+
+private:
+    vector<int> res;
+};
